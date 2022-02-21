207583511abfbf85f18c975d5ef77692b5ae7eb6
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-21 16:22:54 -0300

    lab done

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
+// Instanciando o express em uma variavel
 const express = require("express");
 
+// Definindo a port que vamos usar no localhost 
 const PORT = 4000;
 
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
 
+// Criando uma aplicação express
 const app = express();
+app.use(express.json());
+
+app.get("/pokemon", ((req, res) => {
+   res.status(200).json(allPokemon);
+}))
+
+ app.get("/pokemon/:id", (req, res) => {
+    const {id} = req.params
+
+    const pokemonId = allPokemon.find((currentPokemon) => currentPokemon.id === Number(id));
+
+    return res.status(200).json(pokemonId);
+
+})
+ 
+app.post("/addPokemon", (req, res) => {
+    const idGenerator = Math.floor(Math.random() * (1000 - 2000 + 1) + 2000);
+
+    const newPokemon = {
+        ...req.body,
+        id: idGenerator,
+    }
+    allPokemon.push(newPokemon);
+
+    return res.status(200).json(newPokemon);
+
+})
+
+
+app.put("/pokemon/:id", (req, res) => {
+    const {id} = req.params
+
+    const pokemonEdit = allPokemon.find((currentPokemon) => currentPokemon.id === Number(id))
+
+    allPokemon[allPokemon.indexOf(pokemonEdit)] ={
+        ...pokemonEdit,
+        ...req.body,
+    } 
+
+    return res.status(200).json(allPokemon.find((currentPokemon) => currentPokemon.id === Number(id)))
+})
+
+app.delete('/pokemon/:id', ((req, res) => {
+    const {id} = req.params
+
+    const pokemonDelete = allPokemon.find((currentPokemon) => currentPokemon.id === Number(id))
+
+    allPokemon.splice(allPokemon.indexOf(pokemonDelete), 1)
+
+    return res.status(200).json(pokemonDelete)
+}))
+
+
 
 // -- Define your route listeners here! --
 
